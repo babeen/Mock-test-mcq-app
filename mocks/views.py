27f06945f07d9dc373787f5b655951868293b86a
b@bin_singh_nepali <babinnepalibtl@gmail.com>
2022-05-24 11:06:32 +0545

    not-working-state

--- mocks/views.py
+++ mocks/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from django.core import serializers
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 
 
@@ -14,7 +16,22 @@ def home(request):
 
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+        messages.success(request, 'Your account is successfully signed up.')
+
+        return redirect('signin')
 
 
 def signin(request):
